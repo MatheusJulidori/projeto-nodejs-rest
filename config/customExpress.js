e89e3d7666b43fc147300db27e5f3a18801291d8
Matheus <matheus_julidori@yahoo.com.br>
2021-08-04 12:44:55 -0300

    criando rota post e config app pra receber json

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,10 +1,12 @@
 const express = require('express') //Declaração do express(biblioteca pra criar servidor)
 const consign = require('consign') //importa consign, pra poder agrupar todas as rotas que forem criadas dentro dos controllers
 
-
 module.exports = () => {
 
     const app = express() //Declaração do app rodando express 
+
+    app.use(express.json()) //Configurando decodificador de body json
+    app.use(express.urlencoded({ extended: true })) //Configurando decodificador de body urlencoded
 
     consign()
         .include('controllers') //inclui a pasta controllers com as rotas
--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -2,4 +2,9 @@ module.exports = app => { //Exportando o modulo
 
     app.get('/atendimentos', (req, res) => res.send('rota de atendimentos'))
 
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body)
+        res.send('voce está na rota de atendimentos post')
+    })
+
 }
