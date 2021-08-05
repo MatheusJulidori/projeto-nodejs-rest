1c16e9066e5cfc2e8aa2574f73a5dcbf34c42ff2
Matheus <matheus_julidori@yahoo.com.br>
2021-08-05 15:06:40 -0300

    config MySQL

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 //PARA RODAR, DIGITAR "npm start"
 const customExpress = require('./config/customExpress')
+const conexao = require('./infra/conexao')
 
-const app = customExpress()
 
-app.listen(3000, () => console.log('Servidor rodando na porta 3000')) // mandando o app "escutar"(Rodar) na porta 3000, e logo em seguida
-    // rodando uma função que printa no console
+conexao.connect((erro) => {
+    if (erro) {
+        console.log(erro)
+    } else {
+        console.log("conectado com sucesso")
+
+        const app = customExpress()
+
+        app.listen(3000, () => console.log('Servidor rodando na porta 3000')) // mandando o app "escutar"(Rodar) na porta 3000, e logo em seguida
+
+    }
+})
