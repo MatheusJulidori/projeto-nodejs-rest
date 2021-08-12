b14137a11184df13fb6dc1478b1f69e38ea0851d
Matheus <matheus_julidori@yahoo.com.br>
2021-08-12 10:45:21 -0300

    Trocando buffer pro stream

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,6 +1,6 @@
 const fs = require('fs'); // FS = File System , faremos upload atraves de um buffer
 
-fs.readFile('./assets/1.jpg', (erro, buffer) => { // le o arquivo e transforma em bytes
+/*fs.readFile('./assets/1.jpg', (erro, buffer) => { // le o arquivo e transforma em bytes
     if (erro) {
         console.log(erro)
     } else {
@@ -12,4 +12,12 @@ fs.readFile('./assets/1.jpg', (erro, buffer) => { // le o arquivo e transforma e
                 console.log('arquivo criado')
             }) // cria um arquivo chamado 2.jpg baseado nos bytes do buffer
     }
-})
+})*/
+
+// NO MODO ACIMA, BUFFER É SINCRONO, ELE INTERROMPE A EXECUÇÃO DE CÓDIGOS, ENTÃO USAREMOS STREAM 
+
+fs.createReadStream('./assets/1.jpg') // Uma stream funciona através da emissão de eventos
+    .pipe(fs.createWriteStream('./assets/2.jpg')) // Cria uma stream e pega os valores da stream e joga num 'cano' para poder reescrever a imagem
+    .on('finish', () => {
+        console.log('imagem escrita com sucesso')
+    }) // metodo 'on' captura um evento especifico da stream, nesse caso o finish, e ele permite uma função de callback
